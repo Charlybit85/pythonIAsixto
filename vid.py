74b16ebd0cb5a1df81746a98f80b1eb3b98cf388
CharlyBit <erickperezmendoza5@gmail.com>
2025-03-25 18:19:04 +0200

    picamera2 actualizado

--- vid.py
+++ vid.py
@@ -1,13 +1,19 @@
-import numpy as np
 import cv2
-cap = cv2.VideoCapture(0)
-while(True):
-    # Capture frame-by-frame
-    ret, frame = cap.read()
-    #gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    cv2.imshow('frame',frame)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-# When everything done, release the capture
-cap.release()
-cv2.destroyAllWindows()
+import numpy as np
+from picamera2 import Picamera2
+
+height =480
+width=640
+middle =((width//2),(height//2))
+
+cam = Picamera2()
+
+cam.configure(cam.create_video_configuration(main={"format": 'XRGB8888',
+                                                           "size": (width, height)}))
+cam.start()
+
+while True:
+    frame = cam.capture_array()
+    cv2.circle(frame, middle, 10, (255, 0 , 255), -1)
+    cv2.imshow('f', frame)
+    cv2.waitKey(1)
